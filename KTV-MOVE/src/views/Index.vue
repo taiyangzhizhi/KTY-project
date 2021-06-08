8bbd6c2e3e739eb5f44342829edeeec3db27d0b7
taiyangzhizhi <2258216544@qq.com>
2021-06-08 21:29:28 +0800

    Move第三次提交

--- KTV-MOVE/src/views/Index.vue
+++ KTV-MOVE/src/views/Index.vue
@@ -10,11 +10,187 @@
             <div class="NameTxt">
                 <a href="###">ENVIRONMENTAL&nbsp;DISPLAY</a>
             </div>
-            <div class="text textfont">
-                <p>环境/AMBIENT</p>
+            <div class="txtModule">
+                <div class="text textfont">
+                    <p>环境/AMBIENT</p>
+                </div>
             </div>
             <div class="picture">
                 <img src="../assets/img/20777691_1610085477.jpeg" alt="">
+            </div>
+            <div class="proAttr">
+                <h3>
+                    <a href="###" class="proName urlfont">成都夜总会</a>
+                </h3>
+                <div class="proIntro">
+                    <a href="###" class="textfonts">
+                        联系人：周经理 （夜场资深领队）
+                        <br>
+                        电话：13688143752（微信同号）
+                        <br>
+                        地址：成都
+                    </a>
+                </div>
+            </div>
+            <div class="alonebtn">
+                <a href="tel:18037156030" class="eButObj">
+                    <span class="ficon icon-mobile"></span>
+                    <span class="butText">联系电话</span>
+                </a>
+            </div>
+            <div class="picture bgSetups">
+                <img src="../assets/img/20777700_1610085665.jpeg" alt="">
+            </div>
+            <div class="proAttr">
+                <h3>
+                    <a href="###" class="proName urlfont">成都夜场</a>
+                </h3>
+                <div class="proIntro">
+                    <a href="###" class="textfonts custom">
+                        联系人：周经理 （夜场资深领队）
+                        <br>
+                        电话：13688143752（微信同号）
+                        <br>
+                        地址：成都
+                    </a>
+                </div>
+            </div>
+            <div class="alonebtn">
+                <a href="tel:18037156030" class="eButObj">
+                    <span class="ficon icon-mobile"></span>
+                    <span class="butText">了解更多</span>
+                </a>
+            </div>
+            <div class="picture bgSetups">
+                <img src="../assets/img/20777705_1610085758.jpeg" alt="">
+            </div>
+            <div class="proAttr">
+                <h3>
+                    <a href="###" class="proName urlfont">成都夜场</a>
+                </h3>
+                <div class="proIntro">
+                    <a href="###" class="textfonts custom">
+                        联系人：周经理 （夜场资深领队）
+                        <br>
+                        电话：13688143752（微信同号）
+                        <br>
+                        地址：成都
+                    </a>
+                </div>
+            </div>
+            <div class="alonebtn">
+                <a href="tel:18037156030" class="eButObj">
+                    <span class="ficon icon-mobile"></span>
+                    <span class="butText">了解更多</span>
+                </a>
+            </div>
+            <div class="freeRoom_Mo bgSetup" style="height: 627.5px;">
+                <div class="freeMo">
+                    <div class="max_FreeTxt FreeTxt_pub  editModuleBox">
+                        <a href="###">
+                            <p></p>
+                            <span class="span_pub">
+                                小包低消1080元 中包低消1280 大包低消1380 豪包低消2680全场所有啤酒均可享受买二送一，还有更多豪礼等着你，当晚本包间消费满6666以上，当晚可赠送豪华名宿酒店一套，限当天使用。
+                            </span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="NameTxts">
+                <a href="###">高端KTV</a>
+            </div>
+            <div class="txtModules">
+                <div class="texts textfontsr">
+                    <p>High-end KTV</p>
+                </div>
+            </div>
+            <div class="exhibition_demo">
+                <ul class="demo1">
+                    <li>
+                        <div class="inner list_setup">
+                            <a href="###">
+                                <span class="pic">
+                                    <img src="../assets/img/14385341_1563720196 (1).jpg" alt="">
+                                </span>
+                            </a>
+                            <div class="pic_text">
+                                <a href="###">
+                                    <p class="textfontse text_list_bk">成都夜总会模特</p>
+                                </a>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="inner list_setup">
+                            <a href="###">
+                                <span class="pic">
+                                    <img src="../assets/img/14385336_1563720156 (1).jpg" alt="">
+                                </span>
+                            </a>
+                            <div class="pic_text">
+                                <a href="###">
+                                    <p class="textfontse text_list_bk">成都夜场模特</p>
+                                </a>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="inner list_setup">
+                            <a href="###">
+                                <span class="pic">
+                                    <img src="../assets/img/14385327_1563720105 (1).jpg" alt="">
+                                </span>
+                            </a>
+                            <div class="pic_text">
+                                <a href="###">
+                                    <p class="textfontse text_list_bk">成都酒吧模特</p>
+                                </a>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="inner list_setup">
+                            <a href="###">
+                                <span class="pic">
+                                    <img src="../assets/img/14385309_1563720062 (1).jpg" alt="">
+                                </span>
+                            </a>
+                            <div class="pic_text">
+                                <a href="###">
+                                    <p class="textfontse text_list_bk">成都夜总会模特</p>
+                                </a>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="inner list_setup">
+                            <a href="###">
+                                <span class="pic">
+                                    <img src="../assets/img/14385305_1563720011 (1).jpg" alt="">
+                                </span>
+                            </a>
+                            <div class="pic_text">
+                                <a href="###">
+                                    <p class="textfontse text_list_bk">成都夜场模特</p>
+                                </a>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="inner list_setup">
+                            <a href="###">
+                                <span class="pic">
+                                    <img src="../assets/img/14385288_1563719931 (1).jpg" alt="">
+                                </span>
+                            </a>
+                            <div class="pic_text">
+                                <a href="###">
+                                    <p class="textfontse text_list_bk">成都酒吧模特</p>
+                                </a>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
             </div>
         </div>
         <div class="toolMenuBg" id="tool-menu-bg"></div>
@@ -94,25 +270,225 @@
 @import "../assets/css/common.css";
 @import "../assets/css/reset.css";
     .NameTxt {
-        font-size: 20px;
+        display: block;
+        font-family: SimSun;
         padding-top: 24px;
         background-color: #e1e5e8;
     }
     .NameTxt a {
+        font-size: 22px;
         color: #333;
     }
-    .textfont {
-        font-size: 16px;
-        color: #989898;
-        margin-bottom: 8px;
+    .txtModule {
+        height: 36px;
+        background: url("../assets/img/tmp1538987756_1761174_s.png") no-repeat center bottom;
+        background-color: #e1e5e8;
     }
     .text {
         line-height: 1.75;
+    }
+    .textfont {
+        font-weight: bold;
+        font-family: SimSun;
+        font-size: 16px;
+        color: #989898;
+    }
+    .NameTxts {
+        display: block;
+        font-family: SimSun;
+        padding-top: 24px;
+        background-color: #fff;
+    }
+    .NameTxts a {
+        font-size: 22px;
+        color: #333;
+    }
+    .txtModules {
+        height: 36px;
         background: url("../assets/img/tmp1538987756_1761174_s.png") no-repeat center bottom;
+        background-color: #fff;
+    }
+    .texts {
+        line-height: 1.75;
+    }
+    .textfontsr {
+        font-weight: bold;
+        font-family: SimSun;
+        font-size: 16px;
+        color: #989898;
+    }
+    .picture {
+        border-right-width: 10px;
+        border-right-style: solid;
+        border-left-width: 10px;
+        border-left-style: solid;
+        padding-top: 20px;
+        border-right-color: #e1e5e8;
+        border-left-color: #e1e5e8;
         background-color: #e1e5e8;
     }
     .picture img {
         width: 100%;
         vertical-align: top;
+    }
+    .proAttr {
+        overflow: hidden;
+        border-right-width: 10px;
+        border-right-style: solid;
+        border-left-width: 10px;
+        border-left-style: solid;
+        padding-top: 15px;
+        border-right-color: #e1e5e8;
+        border-left-color: #e1e5e8;
+        background-color: #fff;
+    }
+    .proName {
+        display: block;
+        font-family: 微软雅黑;
+        height: 28px;
+        line-height: 28px;
+        text-overflow: ellipsis;
+        font-size: 16px;
+        overflow: hidden;
+        margin-bottom: 4px;
+    }
+    .proIntro {
+        line-height: 1.5;
+        font-size: 12px;
+        margin-bottom: 5px;
+        word-wrap: break-word;
+    }
+    .textfonts {
+        font-family: 微软雅黑;
+        height: 65px;
+        font-size: 14px;
+        line-height: 24px;
+        color: #585858;
+    }
+    .custom {
+        color: #989898;
+    }
+    .eButObj {
+        display: inline-block;
+        font-family: 微软雅黑;
+        width: 100%;
+        height: 30px;
+        line-height: 30px;
+        color: #fff;
+        background-color: #c00;
+        border-radius: 3px;
+        opacity: 1;
+    }
+    .ficon {
+        display: inline-block;
+        vertical-align: middle;
+        font-size: 20px;
+        color: #fff;
+    }
+    span.ficon {
+        font-family: 'icomoon';
+        speak-as: none;
+        font-style: normal;
+        font-weight: normal;
+        font-variant: normal;
+        text-transform: none;
+        line-height: 1;
+        -webkit-font-smoothing: antialiased;
+    }
+    .icon-mobile:before {
+        content: "\e600";
+    }
+    .butText {
+        font-family: 微软雅黑;
+        font-size: 14px;
+    }
+    .bgSetups {
+        padding-top: 12px;
+    }
+    .bgSetup {
+        background: url("../assets/img/tmp1563767016_1913406_s.jpg") no-repeat center center;
+        background-color: #072c38;
+        margin-top: 5px;
+    }
+    .freeRoom_Mo {
+        position: relative;
+    }
+    .freeMo {
+        position: absolute;
+        left: 2px;
+        top: 225px;
+        z-index: 1;
+    }
+    .freeMo .max_FreeTxt {
+        width: 473px;
+        height: 396px;
+    }
+    .freeMo a {
+        display: block;
+        height: 100%;
+    }
+    .freeMo p {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        color: #333;
+    }
+    .span_pub {
+        font-family: SimSun;
+        color: #fff;
+    }
+    .freeMo span {
+        display: block;
+        height: 100%;
+        line-height: 1.5;
+        font-size: 25.4px;
+        word-wrap: break-word;
+        text-align: left;
+    }
+    .exhibition_demo {
+        font-family: Arial;
+        margin-left: 5px;
+        margin-right: 5px;
+        margin-top: 10px;
+    }
+    .exhibition_demo ul {
+        display: flex;
+        flex-wrap: wrap;
+        width: 100%;
+        overflow: hidden;
+        box-sizing: border-box;
+    }
+    .exhibition_demo ul li {
+        width: 50%;
+        overflow: hidden;
+    }
+    .list_setup {
+        margin-left: 5px;
+        margin-right: 5px;
+    }
+    .exhibition_demo ul li span.pic img {
+        height: 300px;
+        max-width: 100%;
+        max-height: 100%;
+        vertical-align: middle;
+        object-fit: contain;
+    }
+    .exhibition_demo ul li .pic_text {
+        position: relative;
+    }
+    .textfontse {
+        font-size: 16px;
+        color: #555;
+    }
+    .text_list_bk {
+        height: 38px;
+        line-height: 36px;
+    }
+    .exhibition_demo ul li .pic_text p.text_list_bk {
+        position: relative;
+        display: block;
+        overflow: hidden;
+        word-break: break-word;
+        margin-bottom: 2px;
     }
 </style>
